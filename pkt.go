f5a24c6b6004d64dfc8f67e9aa97880d4b8a8641
Rusty Eddy <rusty@rustyeddy.com>
2019-03-23 15:27:54 -0700

    it works basically

--- pkt.go
+++ pkt.go
@@ -1,10 +1,21 @@
 package tlv
 
+import (
+	"io"
+	"log"
+	"strings"
+	"strconv"
+	//"github.com/tarm/serial"
+)
+
 // TLV Structure
 type TLV struct {
 	Type int // Type is agreed on by application
 	Len int // Len is in bytes, includes 2 bytes for header
 	Data []byte // byte slice Len - 2
+
+	Reader io.Reader
+	Writer io.Writer
 }
 
 // NewTLV will create a new data structure set by type.
@@ -19,3 +30,34 @@ func NewTLV(t int) TLV {
 	return tlv
 }
 
+// ReadLoop will loop around reading packets as they arrive
+// sending them to the appriopriate call back
+func ReadLoop(reader io.Reader) error {
+
+	buf := make([]byte, 16)
+	for {
+		n, err := reader.Read(buf)
+		if err != nil && err != io.EOF {
+			log.Println(err)
+		}
+		if n == 0 {
+			continue
+		}
+		incoming := string(buf[:n])
+		incoming = strings.Trim(incoming, "\n\r")
+		parsed := strings.Split(incoming, ":")
+		switch parsed[0] {
+		case "cm":
+			distance := parsed[1]
+			i, err := strconv.Atoi(distance)
+			if err != nil {
+				log.Fatal(err)
+			}
+			//dst := int(distance);
+			log.Println(i)
+
+		default:
+			log.Printf("unknown command %v", parsed[0])
+		}
+	}
+}
--- tlv/main.go
+++ tlv/main.go
@@ -1,1 +1,55 @@
 package main
+
+import "flag"
+import "log"
+import "github.com/tarm/serial"
+import "github.com/mobilerobot-io/tlv"
+
+type Config struct {
+	Port string
+	Baud int
+}
+
+const (
+	Joystick = 0x10
+)
+
+var (
+	config Config
+)
+
+func init() {
+	flag.StringVar(&config.Port, "port", "/dev/tty.wchusbserial1420", "serial port")
+	flag.IntVar(&config.Baud, "baud", 9600, "baud rate")
+}
+
+func main() {
+	flag.Parse()
+	config := &serial.Config{
+		// Name: "/dev/ttyAMA0",
+		Name: config.Port,
+		Baud: config.Baud,
+		ReadTimeout: 1,
+		Size: 8,
+	}
+
+	stream, err := serial.OpenPort(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tlv.AddHandler(Joystick, JoystickCB)
+	tlv.ReadLoop(stream)
+}
+
+func JoystickCB(tlv *TLV) (err error) {
+	if tlv.Type != Joystick {
+		panic("type expected 0x1 got " + string(tlv.Type))
+	}
+	if tlv.Len != 8 {
+		panic("length expected 8 got (%d) " + string(tlv.Len))
+	}
+
+	
+	return nil
+}
